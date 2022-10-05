ba0c2fcf89cd034004e5728019f11185a7610da1
Anca Sarb <sarb.anca@gmail.com>
2022-10-05 20:17:06 +0100

    basic svelte skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<slot />
+
+<style>
+	@import url('https://rsms.me/inter/inter.css');
+
+	/* CSS Reset: https://www.joshwcomeau.com/css/custom-css-reset/ */
+
+	:global(*),
+	:global(*::before),
+	:global(*::after) {
+		box-sizing: border-box;
+	}
+
+	:global(*) {
+		margin: 0;
+	}
+
+	:global(html),
+	:global(body) {
+		height: 100%;
+	}
+
+	:global(body) {
+		line-height: 1.5;
+		-webkit-font-smoothing: antialiased;
+	}
+
+	:global(img),
+	:global(picture),
+	:global(video),
+	:global(canvas),
+	:global(svg) {
+		display: block;
+		max-width: 100%;
+	}
+
+	:global(input),
+	:global(button),
+	:global(textarea),
+	:global(select) {
+		font: inherit;
+	}
+
+	:global(p),
+	:global(h1),
+	:global(h2),
+	:global(h3),
+	:global(h4),
+	:global(h5),
+	:global(h6) {
+		overflow-wrap: break-word;
+	}
+
+	:global(#root),
+	:global(#__next) {
+		isolation: isolate;
+	}
+
+	/* End of CSS reset */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+</main>
+
+<style>
+	main {
+		font-family: 'Inter', sans-serif;
+	}
+</style>
